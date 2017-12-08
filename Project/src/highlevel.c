f16d8b9335abe578a12426d4ae3e747e77e451d3
Cameron Small <camerontsmall@googlemail.com>
2017-12-08 22:42:34 +0000

    highlevel structure

--- Project/src/highlevel.c
+++ Project/src/highlevel.c
@@ -1,6 +1,16 @@
 
-void runhighlevel(void){
+void high_follower(int selection){
+	int looking = 1;
+	int following = 0;
+}
 
+void high_leader(int selection){
+
+	int followed = 0;
 	
-
+	while(getselector() == selection){
+		
+	}
+	
+	
 }
